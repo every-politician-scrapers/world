976ddcbb4a6d4104cc3110a0ddb3775c10532664
Tony Bowden <tony@tmtm.com>
2022-12-05 11:22:07 +0200

    Change approach
    Rather than comparing the two sources with `daff`, join them into a
    side-by-side table for easier eyeballing.

--- sources/foreign/compare.sh
+++ sources/foreign/compare.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
+TMPFILE=$(mktemp)
+
 cd $(dirname $0)
 
 bundle exec ruby scraper.rb |
+  sed -e 's/Q17252/Q17269/g' |
   qsv select country,countryLabel,id,name,positionID,position,startDate |
   qsv rename country,countryLabel,person,name,position,positionLabel,start > scraped.csv
 
 wd sparql -f csv wikidata.js | sed -e 's/T00:00:00Z//g' -e 's#http://www.wikidata.org/entity/##g' | qsv dedup -s psid > wikidata.csv
 
-bundle exec ruby diff.rb | qsv sort -s countrylabel,name | tee diff.csv
+qsv join --left country scraped.csv country wikidata.csv |
+  qsv select "country,countryLabel,person,name,country[1],countryLabel[1],person[1],name[1]" |
+  qsv rename "wpcountry,wpcountrylabel,wpperson,wppersonlabel,wdcountry,wdcountrylabel,wdperson,wdpersonlabel" > $TMPFILE
+
+sqlite3 :memory: -cmd '.mode csv' -cmd ".import $TMPFILE comparison" -cmd '.mode csv' -header '
+  SELECT COALESCE(NULLIF(wpcountry,""),NULLIF(wdcountry,"")) AS country,
+         COALESCE(NULLIF(wpcountrylabel,""),NULLIF(wdcountrylabel,"")) AS countrylabel,
+         wpperson,wppersonlabel,wdperson,wdpersonlabel
+  FROM comparison
+  ORDER BY countrylabel' > comparison.csv
+
+sqlite3 :memory: -cmd '.mode csv' -cmd ".import comparison.csv comparison" -cmd '.mode csv' -header '
+  SELECT * FROM comparison WHERE (wpperson!="" AND wpperson != wdperson) OR (wpperson = "" AND wppersonlabel != wdpersonlabel)
+  ' | qsv fmt > diff.csv
 
 cd ~-
